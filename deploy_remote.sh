af83e7a49e066fe33840143b21647e6fc533d925
Cloe-prog <77048799+Cloe-prog@users.noreply.github.com>
2021-05-19 15:14:05 +0200

    Update deploy_remote.sh

--- deploy_remote.sh
+++ deploy_remote.sh
@@ -5,8 +5,9 @@ if [[$? !=0 ]] then sudo amazon-linux-extras
 install -y java-openjdk11 fi
 
 #On copie les fichiers sur la machine distante
-scp MACHINE_DISTANTE/app
+scp ec2-user@ec2-18-220-191-92.us-east-2.compute.amazonaws.com/app     
 helloWorld.class
 
-#On execute l’application sur la machine distante
-ssh MACHINE_DISTANTE -c ‘cd /app && java helloWorld’
+#On exécute l’application sur la machine distante
+ssh ec2-user@ec2-18-220-191-92.us-east-2.compute.amazonaws.com -c ‘cd /app && java helloWorld’
+
