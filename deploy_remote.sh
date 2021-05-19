2537324be71e5aad699f624275794f44c7deb535
Cloe-prog <77048799+Cloe-prog@users.noreply.github.com>
2021-05-19 15:15:54 +0200

    Update deploy_remote.sh

--- deploy_remote.sh
+++ deploy_remote.sh
@@ -9,5 +9,5 @@ scp ec2-user@ec2-18-220-191-92.us-east-2.compute.amazonaws.com/app
 helloWorld.class
 
 #On exécute l’application sur la machine distante
-ssh ec2-user@ec2-18-220-191-92.us-east-2.compute.amazonaws.com -c ‘cd /app && java helloWorld’
+ssh ec2-user@ec2-18-220-191-92.us-east-2.compute.amazonaws.com -c 'cd /app && java helloWorld'
 
