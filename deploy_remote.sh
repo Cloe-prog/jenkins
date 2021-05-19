a588362f1a89c64f82866268a9e1f178bf2fcf4d
Cloe-prog <77048799+Cloe-prog@users.noreply.github.com>
2021-05-19 15:18:31 +0200

    Update deploy_remote.sh

--- deploy_remote.sh
+++ deploy_remote.sh
@@ -1,8 +1,10 @@
 #!/bin/bash 
 #deploy_remote.sh
 java -version
-if [[$? !=0 ]] then sudo amazon-linux-extras
-install -y java-openjdk11 fi
+if [[$? !=0 ]] 
+then 
+  sudo amazon-linux-extras install -y java-openjdk11 
+fi
 
 #On copie les fichiers sur la machine distante
 scp ec2-user@ec2-18-220-191-92.us-east-2.compute.amazonaws.com/app     
