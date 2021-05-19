911b28708149650d8081b7e80d176f07c5097cf5
Cloe-prog <77048799+Cloe-prog@users.noreply.github.com>
2021-05-19 10:30:52 +0200

    Update deploy_local.sh

--- deploy_local.sh
+++ deploy_local.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #deploy_local.sh
 mkdir –p /app (attention aux droits, il va surement devoir nous attribuer les droits)
-cp HelloWorld.class /app
+cp helloWorld.class /app
 cd /app
 java helloWorld
